3354a9288460a3300c660fba6d671068157438fd
MadhuraB27 <141702251+MadhuraB27@users.noreply.github.com>
2023-09-08 15:07:20 +0530

    Update A2.2.js

--- a22/A2.2.js
+++ a22/A2.2.js
@@ -7,13 +7,14 @@ let imageSelector;
 let selectedImage; 
 
 function setup() {
-  let can=createCanvas(600, 600);
+  let can=createCanvas(700, 500);
   can.parent('column-two');
 
-  imageSelector = createSelect();  
-  imageSelector.option('Tuxedo and norml pooh', 'images/Tuxedo Winnie The Pooh.png');
-  imageSelector.option('oh yes oh no', 'images/oh yes oh no meme.png');
-  imageSelector.option('drake hotline bling', 'images/drake meme.jpg');
+
+  imageSelector = createSelect();
+  imageSelector.option('oh yed oh no', 'images/oh yes oh no meme.png');
+  imageSelector.option('tuxedo vs normal pooh', 'images/Tuxedo Winnie The Pooh.png');
+  imageSelector.option('weak vs strong spongebob', 'images/weak vs strong spongebob.png');
   imageSelector.changed(changeImage);
   imageSelector.parent('column-one');
 
@@ -38,7 +39,6 @@ function setup() {
   
   fontSelector = createRadio();
   fontSelector.option('Impact');
-  fontSelector.option('Comic Sans');
   fontSelector.option('Times New Roman');
   fontSelector.selected('Impact');
   fontSelector.parent('column-one');
@@ -49,65 +49,33 @@ function setup() {
 }
 
 function draw() {
-  background(0,0.0);
+  background(225);
   imageMode(CENTER);
   
   if (selectedImage) {
     image(selectedImage, width / 2, height / 2); 
   }
   
-  
   textSize(fontSizeSlider.value());
   colorMode(RGB);
-  fill(0); // Set text color to black
+  fill(fontColorSliderR.value(), fontColorSliderG.value(), fontColorSliderB.value());
+  textAlign(LEFT, TOP);
   
   let selectedFont = fontSelector.value();
   textFont(selectedFont);
   
-  text(topTextInput.value(), width / 5, 160);
-  text(bottomTextInput.value(), width / 5, height - 240);
+  text(topTextInput.value(), width / 8, 80);
+  text(bottomTextInput.value(), width / 8, height - 190);
 }
 
-function changeImage() {
-  const selectedImagePath = imageSelector.value();
-
-  if (selectedImagePath) {
-    loadImage(selectedImagePath, (img) => {
-      // Callback function to handle image load
-      selectedImage = img;
-      redrawCanvas(); // Redraw the canvas with the new image
-    });
-  }
-}
-
-function redrawCanvas() {
-  background(0); // Clear the canvas with a black background
-
-  if (selectedImage) {
-    image(selectedImage, width / 2, height / 2);
-  }
-
-  textSize(fontSizeSlider.value());
-  colorMode(RGB);
-
-  let r = fontColorSliderR.value();
-  let g = fontColorSliderG.value();
-  let b = fontColorSliderB.value();
-
-  fill(r, g, b);
-  textAlign(CENTER, CENTER);
-
-  let selectedFont = fontSelector.value();
-  textFont(selectedFont);
-
-  text(topTextInput.value(), width / 2, 160);
-  text(bottomTextInput.value(), width / 2, height - 240);
+function generateMeme() {
+  
 }
 
 function changeImage() {
   selectedImage = loadImage(imageSelector.value());
 }
 
-function saveMeme() {
+function generateMeme() {
   saveCanvas('meme', 'png'); // Save the canvas as 'meme.png' 
 }
